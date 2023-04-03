caa294e0113ec7400db3353a727149194a5db705
barba <barba@LAPTOP-AQMH51OP>
2023-04-03 11:04:40 +0100

    Definicao das classes

---
+++ Projeto Jardim-Escola/Projeto Jardim-Escola/Models/Pessoas.cs
@@ -0,0 +1,67 @@
+﻿namespace Projeto_Jardim_Escola.Models
+{       
+    /// <summary>
+    /// Descrição das pessoas
+    /// </summary>
+    public class Pessoas{
+
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        /// <summary>
+        /// nome das pessoas
+        /// </summary>
+        public DateTime DataNascimento { get; set;}
+        /// <summary>
+        /// data de nascimento das pessoas
+        /// </summary>
+
+        public string Morada { get; set; }
+        /// <summary>
+        /// morada das pessoas
+        /// </summary>
+
+        public string CodPostal { get; set; }
+        /// <summary>
+        /// código postal das pessoas
+        /// </summary>
+
+        public int Identificacao { get; set; }
+        /// <summary>
+        /// número de identificação das pessoas
+        /// </summary>
+
+        public string IdentificacaoTipo { get; set; }
+        /// <summary>
+        /// tipo de identificação das pessoas:
+        /// CC - cartão de cidadão
+        /// BI - bilhete de identidade
+        /// P - passaport
+        /// </summary>
+
+        public string NIF { get; set; }
+        /// <summary>
+        /// número de identificção fiscal das pessoas
+        /// </summary>
+
+        public string Telemovel { get; set; }
+        /// <summary>
+        /// número de telemóvel das pessoas
+        /// </summary>
+
+        public string Email { get; set; }
+        /// <summary>
+        /// email das pessoas
+        /// </summary>
+
+        public string Escolariedade { get; set; }
+        /// <summary>
+        /// escolariedade das pessoas
+        /// </summary>
+
+        public string Profissao { get; set; }
+        ///profissão das pessoas
+
+
+    }
+}
