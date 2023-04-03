ed7ac0b786bb0fdebed5b3eb49fbc4e1c498685f
barba <barba@LAPTOP-AQMH51OP>
2023-04-03 10:01:03 +0100

    Versao inicial

---
+++ Projeto Jardim-Escola/Projeto Jardim-Escola/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_Jardim_Escola.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
