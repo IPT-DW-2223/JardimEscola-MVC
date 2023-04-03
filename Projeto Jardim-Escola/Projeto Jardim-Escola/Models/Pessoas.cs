43abe7e091a8e2ff39ed6716abc802c2157e5f14
barba <barba@LAPTOP-AQMH51OP>
2023-04-03 11:16:21 +0100

    alteração

--- Projeto Jardim-Escola/Projeto Jardim-Escola/Models/Pessoas.cs
+++ Projeto Jardim-Escola/Projeto Jardim-Escola/Models/Pessoas.cs
@@ -7,61 +7,64 @@
 
         public int Id { get; set; }
 
-        public string Nome { get; set; }
         /// <summary>
         /// nome das pessoas
         /// </summary>
-        public DateTime DataNascimento { get; set;}
+        public string Nome { get; set; }
+
         /// <summary>
         /// data de nascimento das pessoas
         /// </summary>
-
-        public string Morada { get; set; }
+        public DateTime DataNascimento { get; set;}
+       
         /// <summary>
         /// morada das pessoas
         /// </summary>
-
-        public string CodPostal { get; set; }
+        public string Morada { get; set; }
+       
         /// <summary>
         /// código postal das pessoas
         /// </summary>
-
-        public string Identificacao { get; set; }
+        public string CodPostal { get; set; }
+        
         /// <summary>
         /// número de identificação das pessoas
         /// </summary>
-
-        public string IdentificacaoTipo { get; set; }
+        public string Identificacao { get; set; }
+        
         /// <summary>
         /// tipo de identificação das pessoas:
         /// CC - cartão de cidadão
         /// BI - bilhete de identidade
         /// P - passaport
         /// </summary>
-
-        public string NIF { get; set; }
+        public string IdentificacaoTipo { get; set; }
+        
         /// <summary>
         /// número de identificção fiscal das pessoas
         /// </summary>
-
-        public string Telemovel { get; set; }
+        public string NIF { get; set; }
+        
         /// <summary>
         /// número de telemóvel das pessoas
         /// </summary>
-
-        public string Email { get; set; }
+        public string Telemovel { get; set; }
+        
         /// <summary>
         /// email das pessoas
         /// </summary>
-
-        public string Escolariedade { get; set; }
+        public string Email { get; set; }
+        
         /// <summary>
         /// escolariedade das pessoas
         /// </summary>
+        public string Escolariedade { get; set; }
 
+        /// <summary>
+        /// profissão das pessoas
+        /// </summary>
         public string Profissao { get; set; }
-        ///profissão das pessoas
-
+       
 
     }
 }
