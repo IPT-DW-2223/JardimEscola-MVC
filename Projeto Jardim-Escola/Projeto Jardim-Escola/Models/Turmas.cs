f5a5cc7988f561348d4faaa2c2019fab4dfd1b0b
andredrtsaipt <aluno21296@ipt.pt>
2023-04-03 11:05:51 +0100

    Criação do modelo Turmas

---
+++ Projeto Jardim-Escola/Projeto Jardim-Escola/Models/Turmas.cs
@@ -0,0 +1,25 @@
+﻿namespace Projeto_Jardim_Escola.Models {
+    
+    /// <summary>
+    /// Descrição de uma turma.
+    /// </summary>
+    public class Turmas {
+
+        /// <summary>
+        /// Chave primária da turma.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome da turma.
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Ano letivo da turma.
+        /// </summary>
+        public string AnoLetivo { get; set; }
+
+    }
+
+}
