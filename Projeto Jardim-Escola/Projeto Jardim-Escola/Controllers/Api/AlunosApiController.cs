7dec575492e21e6caaa24473aea9e287710e6436
andredrtsaipt <aluno21296@ipt.pt>
2023-05-24 12:23:14 +0100

    Adicionado API para os alunos

---
+++ Projeto Jardim-Escola/Projeto Jardim-Escola/Controllers/Api/AlunosApiController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projeto_Jardim_Escola.Data;
+
+using Projeto_Jardim_Escola.Data;
+using Projeto_Jardim_Escola.Models;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Projeto_Jardim_Escola.Controllers.Api {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosApiController : ControllerBase {
+
+        private readonly ApplicationDbContext _baseDados;
+
+        public AlunosApiController(ApplicationDbContext baseDados) { _baseDados = baseDados; }
+
+        /// <summary>
+        /// GET: api/AlunosAPI
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AlunoViewModel>>> GetAlunos() {
+            return await _baseDados.Alunos
+                .Include(a => a.Responsavel)
+                .OrderByDescending(a => a.Id)
+                .Select(a => new AlunoViewModel {
+                    Id = a.Id,
+                    Nome = a.Nome,
+                    Identificacao = a.Identificacao,
+                    TipoIdentificacao = a.TipoIdentificacaoFK,
+                    NIF = a.NIF,
+                    DataNascimento = a.DataNascimento,
+                    Responsavel = a.Responsavel.Nome
+                })
+                .ToListAsync();
+        }
+
+    }
+}
---
+++ Projeto Jardim-Escola/Projeto Jardim-Escola/Models/ViewModels.cs
@@ -0,0 +1,15 @@
+﻿namespace Projeto_Jardim_Escola.Models {
+
+    /// <summary>
+    /// Esta classe colecta os dados dos Alunos para a API.
+    /// </summary>
+    public class AlunoViewModel {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Identificacao { get; set; }
+        public int TipoIdentificacao { get; set; }
+        public string NIF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Responsavel { get; set; }
+    }
+}
